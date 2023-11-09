056ab68d9744fdf6a8aa80398bf6c9994160c622
vietqtran <tranquocviet1303@gmail.com>
2023-11-09 20:41:33 +0700

    fix conflict

--- Backend/vietqtran.Api/vietqtran.Api/Controllers/UserController.cs
+++ Backend/vietqtran.Api/vietqtran.Api/Controllers/UserController.cs
@@ -30,7 +30,7 @@ namespace vietqtran.Api.Controllers
 		private readonly IOptions<JwtConfig> _jwtConfig;
 
 
-		public UserController (IAppUserService appUserService, IMapper mapper, ILogger<UserController> logger, IOptions<JwtConfig> jwtConfig)
+		public UserController(IAppUserService appUserService, IMapper mapper, ILogger<UserController> logger, IOptions<JwtConfig> jwtConfig)
 		{
 			_appUserService = appUserService;
 			_mapper = mapper;
@@ -41,7 +41,7 @@ namespace vietqtran.Api.Controllers
 		[HttpGet]
 		[Authorize]
 		[ProducesResponseType(200, Type = typeof(ICollection<AppUserVM>))]
-		public async Task<IActionResult> GetAsync ( )
+		public async Task<IActionResult> GetAsync()
 		{
 			var users = await _appUserService.GetAllUsersServiceAsync();
 			return Ok(_mapper.Map<ICollection<AppUserVM>>(users));
@@ -49,11 +49,12 @@ namespace vietqtran.Api.Controllers
 
 		[HttpPost("register")]
 		[AllowAnonymous]
-		public async Task<IActionResult> Register (SignUpCredentials signUpCredentials)
+		public async Task<IActionResult> Register(SignUpCredentials signUpCredentials)
 		{
 			var result = await _appUserService.Register(signUpCredentials);
 
-			if (result.Status == "Successed") {
+			if (result.Status == "Successed")
+			{
 				return Ok(result);
 			}
 
@@ -62,11 +63,12 @@ namespace vietqtran.Api.Controllers
 
 		[HttpPost("login")]
 		[AllowAnonymous]
-		public async Task<IActionResult> Login (LoginCredentials loginCredentials)
+		public async Task<IActionResult> Login(LoginCredentials loginCredentials)
 		{
 			var result = await _appUserService.Login(loginCredentials);
 
-			if (result.Status == "Seccessed") {
+			if (result.Status == "Seccessed")
+			{
 				return BadRequest(result);
 			}
 
@@ -75,13 +77,14 @@ namespace vietqtran.Api.Controllers
 			var claims = jwtToken.Claims.ToList();
 			var roles = claims.Where(x => x.Type == "role").Select(x => x.Value).ToList();
 
-			return Ok(new {
+			return Ok(new
+			{
 				result,
 				roles
 			});
 		}
 
-		private ClaimsPrincipal ValidateToken (string token)
+		private ClaimsPrincipal ValidateToken(string token)
 		{
 			IdentityModelEventSource.ShowPII = true;
 
