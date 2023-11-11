0d9e2ca18a0e6fb97fa85571a41754f2284ac48a
vietqtran <tranquocviet1303@gmail.com>
2023-11-12 05:42:03 +0700

    move slide settings

--- ClientApp/src/components/pages/Home/story/StoryContainer.tsx
+++ ClientApp/src/components/pages/Home/story/StoryContainer.tsx
@@ -5,50 +5,50 @@ import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 import PrevArrow from "./PrevArrow"
 import NextArrow from "./NextArrow"
+const settings = {
+   dots: false,
+   infinite: false,
+   speed: 500,
+   slidesToShow: 8,
+   slidesToScroll: 3,
+   prevArrow: <PrevArrow />,
+   nextArrow: <NextArrow />,
+   responsive: [
+      {
+         breakpoint: 1311,
+         settings: {
+            slidesToShow: 7,
+         },
+      },
+      {
+         breakpoint: 1180,
+         settings: {
+            slidesToShow: 5.5,
+         },
+      },
+      {
+         breakpoint: 1024,
+         settings: {
+            slidesToShow: 8,
+         },
+      },
+      {
+         breakpoint: 590,
+         settings: {
+            slidesToShow: 7,
+         },
+      },
+      {
+         breakpoint: 500,
+         settings: {
+            slidesToShow: 5,
+         },
+      },
+   ],
+}
 
 function StoryContainer() {
    const slideToShow = 10
-   const settings = {
-      dots: false,
-      infinite: false,
-      speed: 500,
-      slidesToShow: 8,
-      slidesToScroll: 3,
-      prevArrow: <PrevArrow />,
-      nextArrow: <NextArrow />,
-      responsive: [
-         {
-            breakpoint: 1311,
-            settings: {
-               slidesToShow: 7,
-            },
-         },
-         {
-            breakpoint: 1180,
-            settings: {
-               slidesToShow: 5.5,
-            },
-         },
-         {
-            breakpoint: 1024,
-            settings: {
-               slidesToShow: 8,
-            },
-         },
-         {
-            breakpoint: 590,
-            settings: {
-               slidesToShow: 7,
-            },
-         },
-         {
-            breakpoint: 500,
-            settings: {
-               slidesToShow: 5,
-            },
-         },
-      ],
-   }
    return (
       <div className='w-full'>
          <div className='w-auto overflow-hidden'>
