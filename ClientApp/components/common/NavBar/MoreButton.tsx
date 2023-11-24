0751a56cf567ceef8a69625110629c22bf418f21
vietqtran <tranquocviet1303@gmail.com>
2023-11-24 17:32:37 +0700

    More menu

--- ClientApp/components/common/NavBar/MoreButton.tsx
+++ ClientApp/components/common/NavBar/MoreButton.tsx
@@ -2,6 +2,9 @@ import React, { useState } from "react"
 
 import MoreOutline from "@components/Icons/More/MoreOutline"
 import MoreSolid from "@components/Icons/More/MoreSolid"
+import { logoutRedux } from "@redux/actions/user"
+import { useDispatch } from "react-redux"
+import { useRouter } from "next/navigation"
 
 interface MoreButtonProps {
    tab: string
@@ -9,29 +12,56 @@ interface MoreButtonProps {
 
 function MoreButton({ tab }: MoreButtonProps) {
    const [clicked, setClicked] = useState(false)
+   const dispatch = useDispatch()
+   const router = useRouter()
+
+   const handleLogOut = () => {
+      dispatch(logoutRedux())
+      router.push("/a/login")
+   }
+
    return (
-      <div
-         onClick={() => {
-            setClicked(!clicked)
-         }}
-         className={`group my-3 w-full rounded-lg duration-200 ease-linear hover:bg-gray-100`}
-      >
-         <div className='cursor-pointer hover:text-black'>
-            <div className='flex items-center justify-start p-3'>
-               <div className='block w-[40px] group-hover:scale-105'>
-                  {clicked ? <MoreSolid /> : <MoreOutline />}
-               </div>
-               <div
-                  className={`${
-                     tab === "search" || tab === "notification"
-                        ? "hidden"
-                        : "block"
-                  }`}
-               >
-                  <span className='hidden lg:block'>More</span>
+      <div className='w-full relative'>
+         <div
+            onClick={() => {
+               setClicked(!clicked)
+            }}
+            className={` group my-3 w-full rounded-lg duration-200 ease-linear hover:bg-gray-100`}
+         >
+            <div className='cursor-pointer hover:text-black'>
+               <div className='flex items-center justify-start p-3'>
+                  <div className='block w-[40px] group-hover:scale-105'>
+                     {clicked ? <MoreSolid /> : <MoreOutline />}
+                  </div>
+                  <div
+                     className={`${
+                        tab === "search" || tab === "notification"
+                           ? "hidden"
+                           : "block"
+                     }`}
+                  >
+                     <span className='hidden lg:block'>More</span>
+                  </div>
                </div>
             </div>
          </div>
+         {clicked && (
+            <div className='bottom-[70px] rounded-lg absolute menu-shadow w-[266px] text-sm'>
+               <div className='p-2 border-b-2 border-b-gray-100'>
+                  <div className='p-3 rounded-lg hover:bg-gray-100 cursor-pointer'>
+                     Switch Account
+                  </div>
+               </div>
+               <div className='p-2'>
+                  <div
+                     onClick={handleLogOut}
+                     className='p-3 rounded-lg hover:bg-gray-100 cursor-pointer'
+                  >
+                     Log Out
+                  </div>
+               </div>
+            </div>
+         )}
       </div>
    )
 }
